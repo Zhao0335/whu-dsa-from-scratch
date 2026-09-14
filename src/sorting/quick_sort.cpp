f23d59109cdb2885af88e1599933672e94cecb9f
Aurora <3797723137@qq.com>
2026-09-15 00:47:12 +0800

    修正无 STL 检查误报

--- src/sorting/quick_sort.cpp
+++ src/sorting/quick_sort.cpp
@@ -1,6 +1,6 @@
 #include "whudsa/sorting/quick_sort.hpp"
 #include <stdexcept>
 namespace whudsa {
-// CORE TODO：分区 + 递归。不要调用 std::sort。
+// CORE TODO：分区 + 递归。禁止调用标准库排序函数。
 void quickSort(int* a, int n) { throw std::logic_error("TODO: quickSort"); }
 }
